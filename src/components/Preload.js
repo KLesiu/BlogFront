7887a3f10e80b6476f91e2c32bbe8cb892c86016
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-07-28 18:51:58 +0200

    Initial

---
+++ src/components/Preload.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const Preload=(props)=>{
+    return(
+        <div className="preloadContainer">
+            <button>JOIN US!</button>
+        </div>
+    )
+}
+export default Preload
