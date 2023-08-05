acb6c7233bc8cfea562158b6288913d633672049
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-08-05 12:13:30 +0200

    Add function that shows user only published posts, and shows admin all posts

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -4,6 +4,8 @@ import uniqid from 'uniqid'
 
 const Blog =(props)=>{
     const [posts,setPosts]= useState([])
+    const [adminPosts,setAdminPosts]= useState([])
+    const [admin,setAdmin]=useState('')
     const [welcome,setWelcome]= useState(false)
     const [list,setList]= useState('')
     const getPosts = async()=>{
@@ -13,16 +15,47 @@ const Blog =(props)=>{
         }).then(function(data){
             const count = data.length
             const array = []
-            console.log(data)
+            const adminArray = []
+            
             for(let i = 0;i<count;i++){
+                if(data[i].published ===true){
                 array.push(data[i])
+                }
+                adminArray.push(data[i])
 
             }
             setPosts(array)
+            setAdminPosts(adminArray)
             setWelcome(true)
-            return console.log(posts)
+            return 
         })
     }
+    const isAdmin=async()=>{
+       
+        const id = localStorage.getItem("id")
+        
+        const response = await fetch('http://localhost:3001/api/auth/isAdmin',{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                _id:id 
+            })
+        }).then(function(res){
+            return res.json()
+        }).then(function(res){
+            const admin = res.admin
+            setAdmin(admin)
+            return admin
+        })
+        
+       
+        
+    }
+    useEffect(()=>{
+        isAdmin()
+    },[])
 
     if(welcome===false){
     return(
@@ -33,20 +66,38 @@ const Blog =(props)=>{
         )
     }
     if(welcome===true){
-        return(
-            <div id="postCardsContainer">
-                {posts.map((element)=>{
-                    return (
-                        <a href={`/blog/${element._id}`} className="postCard"><div  key={uniqid()}>
-                        <h3>{element.title}</h3>
-                        <p>{element.body}</p>
-                        <p>Created: {element.createdAt}</p>
-                        <p>Last update: {element.updatedAt}</p>
-                    </div></a>  
-                    )
-                })}
-            </div>
-        )
+        if(admin===false){
+            return(
+                <div id="postCardsContainer">
+                    {posts.map((element)=>{
+                        return (
+                            <a href={`/blog/${element._id}`} className="postCard"><div  key={uniqid()}>
+                            <h3>{element.title}</h3>
+                            <p>{element.body}</p>
+                            <p>Created: {element.createdAt}</p>
+                            <p>Last update: {element.updatedAt}</p>
+                        </div></a>  
+                        )
+                    })}
+                </div>
+            )
+        }else if(admin===true){
+            return(
+                <div id="postCardsContainer">
+                    {adminPosts.map((element)=>{
+                        return (
+                            <a href={`/blog/${element._id}`} className="postCard"><div  key={uniqid()}>
+                            <h3>{element.title}</h3>
+                            <p>{element.body}</p>
+                            <p>Created: {element.createdAt}</p>
+                            <p>Last update: {element.updatedAt}</p>
+                        </div></a>  
+                        )
+                    })}
+                </div>
+            )
+        }
+       
     }
     
 }
